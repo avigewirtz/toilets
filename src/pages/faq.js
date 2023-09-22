a622da397e60b203566294577c58748091048784
Avi Gewirtz <sgewirtz@princeton.edu>
2023-09-21 21:31:58 -0400

    highrise issues

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -20,93 +20,53 @@ const FaqPage = (props) => {
           <div className={styles.section}>
             <span>Your Orders</span>
             <div className={styles.subSection}>
-              <h3>Deliveries</h3>
+              <h3>Reservations and Deliveries</h3>
               <p>
-                To review the status of your order please visit the "My Orders"
-                section of your Sunspel Account. You should receive an email
-                notification when your package is dispatched. Please check your
-                email inbox and your junk filter.
+                To review the status of your reservation, please visit the "My Orders"
+                section of your account. You should receive an email notification when
+                your porta potty is dispatched for delivery.
               </p>
               <p>
-                We dispatch all orders within 24 hours of being placed. During
-                our sale periods it can get very busy in our factory, orders may
-                take a little longer to be dispatched so please add five working
-                days just in case.
+                We aim to dispatch all units within 48 hours of the confirmed reservation
+                time. However, during peak seasons or events, deliveries may take longer.
               </p>
               <p>
-                If there’s something you want to know about your order, please
-                get in touch with us at customerservice@example.com or call us
-                on +44 (0)115 111 1111.
+                If you have questions about your order, please contact us at
+                support@PortaPottyRental.com or call us on (555) 123-4567.
               </p>
             </div>
             <div className={styles.subSection}>
               <h3>Returns & Exchanges</h3>
               <p>
-                To review the status of your order please visit the "My Orders"
-                section of your Sunspel Account. You should receive an email
-                notification when your package is dispatched. Please check your
-                email inbox and your junk filter.
-              </p>
-              <p>
-                We dispatch all orders within 24 hours of being placed. During
-                our sale periods it can get very busy in our factory, orders may
-                take a little longer to be dispatched so please add five working
-                days just in case.
+                We accept cancellations and changes up to 24 hours before the scheduled
+                delivery time. Please note that cancellations or changes within 24 hours
+                may incur a fee.
               </p>
             </div>
           </div>
 
           <div className={styles.section}>
-            <span>Payment</span>
+            <span>Payment and Billing</span>
             <div className={styles.subSection}>
-              <h3>Shipping Rates</h3>
+              <h3>Rental Rates</h3>
               <p>
-                To review the status of your order please visit the "My Orders"
-                section of your Sunspel Account. You should receive an email
-                notification when your package is dispatched. Please check your
-                email inbox and your junk filter.
-              </p>
-              <p>
-                We dispatch all orders within 24 hours of being placed. During
-                our sale periods it can get very busy in our factory, orders may
-                take a little longer to be dispatched so please add five working
-                days just in case.
-              </p>
-              <p>
-                If there’s something you want to know about your order, please
-                get in touch with us at customerservice@example.com call us on
-                +44 (0)115 111 1111.
+                Our rental rates are competitive and are calculated based on the duration
+                and type of unit rented. Discounts are available for long-term rentals.
               </p>
             </div>
             <div className={styles.subSection}>
-              <h3>Currency</h3>
+              <h3>Accepted Payment Methods</h3>
               <p>
-                Our website automatically selects the currency that matches your
-                delivery location. You can change this manually in the top left
-                hand corner of the page.
-              </p>
-              <p>
-                For deliveries to the UK, we will bill you in Pounds Sterling
-                (£), to the European Union in Euros (€), to the US in Dollars
-                ($), and to the Rest of World in Pounds Sterling (£).
-              </p>
-              <p>
-                Standard rate VAT is included in all orders to the UK and
-                Europe. Orders to the Rest of the World do not include VAT.
-                Where applicable, you will be responsible for any local country
-                import duty.
+                We accept all major credit and debit cards, as well as electronic bank
+                transfers.
               </p>
             </div>
             <div className={styles.subSection}>
               <h3>Suspect Fraud?</h3>
               <p>
-                Given our high levels of security, it’s very unlikely fraudulent
-                use of your card will take place on our website. But if a
-                fraudulent transaction does take place, first contact your
-                credit or debit card company so they can protect your card and
-                reimburse you. Then let us know at customerservice@example.com
-                and we will work with your card company to minimise any further
-                inconvenience to you.
+                If you suspect a fraudulent transaction involving our services, please
+                first contact your bank to secure your account, and then notify us at
+                support@PortaPottyRental.com.
               </p>
             </div>
           </div>
