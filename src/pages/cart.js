017ad6c61911436e2d9cf82754c4b8af2d693735
Avi Gewirtz <sgewirtz@princeton.edu>
2023-09-22 02:59:03 -0400

    tried fixing cart.js undefined issue

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -15,6 +15,39 @@ import * as styles from './CSS/cart.module.css';
 const CartPage = (props) => {
   const cartContext = useContext(CartContext);  // Use CartContext
 
+  if (!cartContext || !cartContext.cart) {
+    // Handle the case where cartContext or cartContext.cart is undefined
+    return (
+      <div>
+        <div className={styles.contentContainer}>
+          <Container size={'large'} spacing={'min'}>
+            <div className={styles.headerContainer}>
+              <div className={styles.shoppingContainer}>
+                <Link className={styles.shopLink} to={'/shop'}>
+                  <Icon symbol={'arrow'}></Icon>
+                  <span className={styles.continueShopping}>
+                    Continue Shopping
+                  </span>
+                </Link>
+              </div>
+              <Brand />
+              <div className={styles.loginContainer}>
+                <Link to={'/login'}>Login</Link>
+              </div>
+            </div>
+            <div className={styles.summaryContainer}>
+              <h3>My Bag</h3>
+              <div className={styles.cartContainer}>
+                <p>Your cart is empty.</p>
+              </div>
+            </div>
+          </Container>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+
   return (
     <div>
       <div className={styles.contentContainer}>
@@ -36,12 +69,11 @@ const CartPage = (props) => {
           <div className={styles.summaryContainer}>
             <h3>My Bag</h3>
             <div className={styles.cartContainer}>
-              <div className={styles.cartItemsContainer}>
-                { 
-                  cartContext.cart.map((item, index) => <CartItem key={index} {...item} />) 
-                  // Replace index with a unique identifier if available
-                }
-              </div>
+              {cartContext.cart.length > 0 ? (
+                cartContext.cart.map((item, index) => <CartItem key={index} {...item} />)
+              ) : (
+                <p>Your cart is empty.</p>
+              )}
               <OrderSummary />
             </div>
           </div>
